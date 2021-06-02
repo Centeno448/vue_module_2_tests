2fde3229d0e8a605b10c10cac7ea2711a083396d
Centeno448 <diego.centeno@udla.edu.ec>
2021-06-01 19:42:35 -0500

    fixed tests

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,16 +1,18 @@
 import { shallowMount } from "@vue/test-utils";
 import App from "@/App.vue";
 
-test('App.vue muestra el titulo de las notas dentro de una lista | Asegúrate de que App.vue defina en su función data la propiedad "notas", y que el titulo de los elementos sea desplegado en una lista', () => {
-  const notas = [{ titulo: "testing 12" }, { titulo: "testing 12" }];
+test('App.vue muestra el titulo de las notas dentro de una lista | Asegúrate de que App.vue defina en su función data la propiedad "notas", y que el titulo de los elementos sea desplegado en una lista', async () => {
+  const notas = [{ titulo: "testing 12" }, { titulo: "testing 13" }];
 
   const wrapper = shallowMount(App, {
     data() {
       return {
-        notas
+        notas: []
       };
     }
   });
+
+  await wrapper.setData({notas});
 
   const listItems = wrapper.findAll("li");
 
@@ -18,9 +20,11 @@ test('App.vue muestra el titulo de las notas dentro de una lista | Asegúrate de
     const item = listItems[i];
     expect(item.text()).toBe(notas[i].titulo);
   }
+
+  expect(listItems.length).toBe(2);
 });
 
-test('App.vue muestra "No hay notas guardadas" cuando el arreglo de notas está vacío | Asegúrate de que App.vue muestre un <p> con "No hay notas guardadas" cuando el arreglo de notas se encuentre vacío', () => {
+test('App.vue muestra "No hay notas guardadas" cuando el arreglo de notas está vacío | Asegúrate de que App.vue muestre un <p> con "No hay notas guardadas" cuando el arreglo de notas se encuentre vacío', async () => {
   const notas = [];
 
   const wrapper = shallowMount(App, {
@@ -30,6 +34,8 @@ test('App.vue muestra "No hay notas guardadas" cuando el arreglo de notas está 
       };
     }
   });
+
+  await wrapper.setData({notas})
 
   const p = wrapper.get("p");
 
